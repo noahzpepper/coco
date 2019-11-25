d8c3820c1140c7fad6ae8ad099016a70986881a9
noahzpepper <noahzpepper@berkeley.edu>
2019-11-24 17:46:13 -0800

    splash screen initial impl

--- app/src/main/java/com/coco/coco/MainActivity.java
+++ app/src/main/java/com/coco/coco/MainActivity.java
@@ -3,6 +3,12 @@ package com.coco.coco;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ((TextView) findViewById(R.id.textView)).setText(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getEmail());
+        findViewById(R.id.textView).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+            }
+        });
+
     }
 }
