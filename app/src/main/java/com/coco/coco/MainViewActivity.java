8fec8055c89a759a416a173d8e00c2c1ae98c0e0
noahzpepper <noahzpepper@berkeley.edu>
2019-11-26 15:54:14 -0800

    Link all screens together

--- app/src/main/java/com/coco/coco/MainViewActivity.java
+++ app/src/main/java/com/coco/coco/MainViewActivity.java
@@ -1,14 +1,22 @@
 package com.coco.coco;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 import com.google.android.material.tabs.TabLayout;
 
-public class MainViewActivity extends AppCompatActivity {
+public class MainViewActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "MAIN";
+
+    ImageView accountButton, filterButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,5 +37,24 @@ public class MainViewActivity extends AppCompatActivity {
         viewPager.setAdapter(tabsAdapter);
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
 
+        accountButton = findViewById(R.id.mainAccountButton);
+        filterButton = findViewById(R.id.mainFilterButton);
+
+        //mainIncluded.setOnClickListener(this);
+        accountButton.setOnClickListener(this);
+        filterButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Log.d(TAG, "" + String.format("onClick: %d", view.getId()));
+        switch (view.getId()) {
+            case R.id.mainFilterButton:
+                startActivity(new Intent(MainViewActivity.this, UserFilterActivity.class));
+                break;
+            case R.id.mainAccountButton:
+                startActivity(new Intent(MainViewActivity.this, UserAccountActivity.class));
+                break;
+        }
     }
 }
--- app/src/main/java/com/coco/coco/UserFilterActivity.java
+++ app/src/main/java/com/coco/coco/UserFilterActivity.java
@@ -3,6 +3,7 @@ package com.coco.coco;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class UserFilterActivity extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class UserFilterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_filter);
+
+        findViewById(R.id.userFilterBackButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 }
